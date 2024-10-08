376b282d84401ea643427e5c93cf71cc4abf74ed
shubhamchute <69863014+shubhamchute@users.noreply.github.com>
2024-10-09 00:15:38 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "5. please visit my website learning-ocean.com";
+        return "My name is Munna , Shubham and Ashish are my friends and application is working";
     }
 
 }
